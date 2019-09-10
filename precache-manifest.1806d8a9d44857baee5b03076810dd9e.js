85b73ef79a1b7ee4f709aeded964785533bf7357
Ren.Yi <ren.yi@atlasp.io>
2019-09-10 13:44:53 +0800

    init

---
+++ precache-manifest.1806d8a9d44857baee5b03076810dd9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe37040a6e4ae41e9112fa297ed053a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "843a7112c00069ade4ec",
+    "url": "./static/css/2.99531e7f.chunk.css"
+  },
+  {
+    "revision": "9fc93f797e2e884c938b",
+    "url": "./static/css/main.b2a8b7c0.chunk.css"
+  },
+  {
+    "revision": "843a7112c00069ade4ec",
+    "url": "./static/js/2.18b69b4b.chunk.js"
+  },
+  {
+    "revision": "9fc93f797e2e884c938b",
+    "url": "./static/js/main.8cc0de2d.chunk.js"
+  },
+  {
+    "revision": "ece3c6ab17d3ac4f5dbf",
+    "url": "./static/js/runtime~main.83f36274.js"
+  }
+]);
